d49c2c80b3ba4d37cbd137e9dcecab77e2561bcb
cabdellah <104766976+cabdellah@users.noreply.github.com>
2022-05-02 18:24:50 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,443 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        drawer: Drawer(),
-        appBar: AppBar(
-          title: Text('بسم الله الرحمن الرحيم'),
-          centerTitle: true,
-          actions: [
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body: Container(color: Colors.orange),
-      ),
-    );
+[
+  { 
+    "id": "49",
+    "title": "الإذاعة الأولى",
+    "subtitle": "إذاعة إخبارية عامة",
+    "urlImage":"assets/images/chaine_1.png",
+    "urlRadio":"https://webradio.tda.dz/Chaine1_64K.mp3"
+  },
+  { 
+    "id": "50",
+    "title": "الإذاعة الثانية",
+    "subtitle": "إذاعة ناطقة بالأمازيغية عامة",
+    "urlImage":"assets/images/chaine_2.png",
+    "urlRadio":"https://webradio.tda.dz/Chaine2_64K.mp3"
+  },
+  { 
+    "id": "51",
+    "title": "الإذاعة الثالثة",
+    "subtitle": "إذاعة ناطقة بالفرنسية عامة ",
+    "urlImage":"assets/images/chaine_3.png",
+    "urlRadio":"https://webradio.tda.dz/Chaine3_64K.mp3"
+  },
+  { 
+    "id": "52",
+    "title": "الإذاعة الدولية",
+    "subtitle": "إذاعة إخبارية دولية",
+    "urlImage":"assets/images/inter.png",
+    "urlRadio":"https://webradio.tda.dz/Internationale_64K.mp3"
+  },
+  { 
+    "id": "53",
+    "title": "إذاعة القرآن الكريم",
+    "subtitle": "إذاعة القرآن الكريم",
+    "urlImage":"assets/images/Quoran.png",
+    "urlRadio":"https://webradio.tda.dz/Coran_64K.mp3"
+  },
+  { 
+    "id": "54",
+    "title": "الإذاعة الثقافية",
+    "subtitle": "إذاعة الثقافة و الفن",
+    "urlImage":"assets/images/culture.png",
+    "urlRadio":"https://webradio.tda.dz/Culture_64K.mp3"
+  },
+  { 
+    "id": "55",
+    "title": "إذاعة جيل أف أم",
+    "subtitle": "إذاعة شبابية متنوعة",
+    "urlImage":"assets/images/jilfm.png",
+    "urlRadio":"https://webradio.tda.dz/Jeunesse_64K.mp3"
+  },
+  { 
+    "id": "56",
+    "title": "إذاعة البهجة",
+    "subtitle": "إذاعة ثقافية فنية عامة",
+    "urlImage":"assets/images/Bahdja.png",
+    "urlRadio":"https://webradio.tda.dz/ElBahdja_64K.mp3"
+  },
+  { 
+    "id": "56",
+    "title": "راديو الشعبي",
+    "subtitle": "إذاعة أغاني الشعبي",
+    "urlImage":"assets/images/DZ_echaabia.png",
+    "urlRadio":"https://radio-dzair.net/proxy/chaabia?mp=/stream/1/"
+  },
+  { 
+    "id": "56",
+    "title": "راديو الشرقي",
+    "subtitle": "إذاعة أغاني الشرقي",
+    "urlImage":"assets/images/DZ_orientale.png",
+    "urlRadio":"https://radio-dzair.net/proxy/orientale?mp=/stream/1/"
+  },{ 
+    "id": "56",
+    "title": "راديو الأوراس",
+    "subtitle": "إذاعة أغاني الأوراسي",
+    "urlImage":"assets/images/DZ_aures.png",
+    "urlRadio":"https://radio-dzair.net/proxy/aures?mp=/stream/1/"
+  },
+  { 
+    "id": "56",
+    "title": "راديو الأندلسي",
+    "subtitle": "إذاعة أغاني الأندلسي",
+    "urlImage":"assets/images/DZ_andaloussia.png",
+    "urlRadio":"https://radio-dzair.net/proxy/andaloussia?mp=/stream/1/"
+  },
+  { 
+    "id": "56",
+    "title": "راديو الصحارى",
+    "subtitle": "إذاعة أغاني الصحراوي",
+    "urlImage":"assets/images/DZ_sahara.png",
+    "urlRadio":"https://radio-dzair.net/proxy/sahara?mp=/stream/1/"
+  },
+  { 
+    "id": "56",
+    "title": "راديو راينا",
+    "subtitle": "إذاعة أغاني الدزيري",
+    "urlImage":"assets/images/DZ_raina.png",
+    "urlRadio":"https://radio-dzair.net/proxy/dzair?mp=/stream/1/"
+  },
+  { 
+    "id": "56",
+    "title": "راديو إيزورن",
+    "subtitle": "إذاعة أغاني القبائلي",
+    "urlImage":"assets/images/DZ_izuran.png",
+    "urlRadio":"https://radio-dzair.net/proxy/izuran?mp=/stream/1/"
+  },
+  { 
+    "id": "01",
+    "title": "إذاعة أدرار",
+    "subtitle": "01 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_01.png",
+    "urlRadio":"https://webradio.tda.dz/Adrar_64K.mp3"
+  },
+  { 
+    "id": "02",
+    "title": "إذاعة الشلف",
+    "subtitle": "02 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_02.png",
+    "urlRadio":"https://webradio.tda.dz/Chlef_64K.mp3"
+  },
+  { 
+    "id": "03",
+    "title": "إذاعة الأغواط",
+    "subtitle": "03 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_03.png",
+    "urlRadio":"https://webradio.tda.dz/Laghouat_64K.mp3"
+  },
+  { 
+    "id": "04",
+    "title": "إذاعة أم البواقي",
+    "subtitle": "04 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_04.png",
+    "urlRadio":"https://webradio.tda.dz/OumBouaghi_64K.mp3"
+  },
+  { 
+    "id": "05",
+    "title": "إذاعة باتنة",
+    "subtitle": "05 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_05.png",
+    "urlRadio":"https://webradio.tda.dz/Batna_64K.mp3"
+  },
+  { 
+    "id": "06",
+    "title": "إذاعة بجاية",
+    "subtitle": "06 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_06.png",
+    "urlRadio":"https://webradio.tda.dz/Bejaia_64K.mp3"
+  },
+  { 
+    "id": "07",
+    "title": "إذاعة بسكرة",
+    "subtitle": "07 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_07.png",
+    "urlRadio":"https://webradio.tda.dz/Biskra_64K.mp3"
+  },
+  { 
+    "id": "08",
+    "title": "إذاعة بشار",
+    "subtitle": "08 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_08.png",
+    "urlRadio":"https://webradio.tda.dz/Bechar_64K.mp3"
+  },
+  { 
+    "id": "09",
+    "title": "إذاعة البليدة",
+    "subtitle": "09 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_09.png",
+    "urlRadio":"https://webradio.tda.dz/Blida_64K.mp3"
+  },
+  { 
+    "id": "10",
+    "title": "إذاعة البويرة",
+    "subtitle": "10 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_10.png",
+    "urlRadio":"https://webradio.tda.dz/Bouira_64K.mp3"
+  },
+  { 
+    "id": "11",
+    "title": "إذاعة تمنراست",
+    "subtitle": "11 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_11.png",
+    "urlRadio":"https://webradio.tda.dz/Tamanrasset_64K.mp3"
+  },
+  { 
+    "id": "12",
+    "title": "إذاعة تبسة",
+    "subtitle": "12 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_12.png",
+    "urlRadio":"https://webradio.tda.dz/Tebessa_64K.mp3"
+  },
+  { 
+    "id": "13",
+    "title": "إذاعة تلمسان",
+    "subtitle": "13 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_13.png",
+    "urlRadio":"https://webradio.tda.dz/Tlemcen_64K.mp3"
+  },
+  { 
+    "id": "14",
+    "title": "إذاعة تيارت",
+    "subtitle": "14 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_14.png",
+    "urlRadio":"https://webradio.tda.dz/Tiaret_64K.mp3"
+  },
+  { 
+    "id": "15",
+    "title": "إذاعة تيزي وزو",
+    "subtitle": "15 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_15.png",
+    "urlRadio":"https://webradio.tda.dz/TiziOuzou_64K.mp3"
+  },
+  { 
+    "id": "16",
+    "title": "إذاعة الجزائر",
+    "subtitle": "16 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_16.png",
+    "urlRadio":"https://radio-dzair.net/proxy/dzair?mp=/stream/1/"
+  },
+  { 
+    "id": "17",
+    "title": "إذاعة الجلفة",
+    "subtitle": "17 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_17.png",
+    "urlRadio":"https://webradio.tda.dz/Djelfa_64K.mp3"
+  },
+  { 
+    "id": "18",
+    "title": "إذاعة جيجل",
+    "subtitle": "18 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_18.png",
+    "urlRadio":"https://webradio.tda.dz/Jijel_64K.mp3"
+  },
+  { 
+    "id": "19",
+    "title": "إذاعة سطيف",
+    "subtitle": "19 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_19.png",
+    "urlRadio":"https://webradio.tda.dz/Setif_64K.mp3"
+  },
+  { 
+    "id": "20",
+    "title": "إذاعة سعيدة",
+    "subtitle": "20 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_20.png",
+    "urlRadio":"https://webradio.tda.dz/Saida_64K.mp3"
+  },
+  { 
+    "id": "21",
+    "title": "إذاعة سكيكدة",
+    "subtitle": "21 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_21.png",
+    "urlRadio":"https://webradio.tda.dz/Skikda_64K.mp3"
+  },
+  { 
+    "id": "22",
+    "title": "إذاعة سيدي بلعباس",
+    "subtitle": "22 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_22.png",
+    "urlRadio":"https://webradio.tda.dz/SidiBelabes_64K.mp3"
+  },
+  { 
+    "id": "23",
+    "title": "إذاعة عنابة",
+    "subtitle": "23 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_23.png",
+    "urlRadio":"https://webradio.tda.dz/Annaba_64K.mp3"
+  },
+  { 
+    "id": "24",
+    "title": "إذاعة قالمة",
+    "subtitle": "24 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_24.png",
+    "urlRadio":"https://webradio.tda.dz/Guelma_64K.mp3"
+  },
+  { 
+    "id": "25",
+    "title": "إذاعة قسنطينة",
+    "subtitle": "25 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_25.png",
+    "urlRadio":"https://webradio.tda.dz/Constantine_64K.mp3"
+  },
+  { 
+    "id": "26",
+    "title": "إذاعة المدية",
+    "subtitle": "26 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_26.png",
+    "urlRadio":"https://webradio.tda.dz/Medea_64K.mp3"
+  },
+  { 
+    "id": "27",
+    "title": "إذاعة مستغانم",
+    "subtitle": "27 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_27.png",
+    "urlRadio":"https://webradio.tda.dz/Mostaganem_64K.mp3"
+  },
+  { 
+    "id": "28",
+    "title": "إذاعة المسيلة",
+    "subtitle": "28 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_28.png",
+    "urlRadio":"https://webradio.tda.dz/Msila_64K.mp3"
+  },
+  { 
+    "id": "29",
+    "title": "إذاعة معسكر",
+    "subtitle": "29 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_29.png",
+    "urlRadio":"https://webradio.tda.dz/Mascara_64K.mp3"
+  },
+  { 
+    "id": "30",
+    "title": "إذاعة ورقلة",
+    "subtitle": "30 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_30.png",
+    "urlRadio":"https://webradio.tda.dz/Ouargla_64K.mp3"
+  },
+  { 
+    "id": "31",
+    "title": "إذاعة وهران",
+    "subtitle": "31 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_31.png",
+    "urlRadio":"https://webradio.tda.dz/Oran_64K.mp3"
+  },
+  { 
+    "id": "32",
+    "title": "إذاعة البيض",
+    "subtitle": "32 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_32.png",
+    "urlRadio":"https://webradio.tda.dz/ElBayedh_64K.mp3"
+  },
+  { 
+    "id": "33",
+    "title": "إذاعة إليزي",
+    "subtitle": "33 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_33.png",
+    "urlRadio":"https://webradio.tda.dz/Illizi_64K.mp3"
+  },
+  { 
+    "id": "34",
+    "title": "إذاعة برج بوعريريج",
+    "subtitle": "34 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_34.png",
+    "urlRadio":"https://webradio.tda.dz/BordjBouArreridj_64K.mp3"
+  },
+  { 
+    "id": "35",
+    "title": "إذاعة بومرداس",
+    "subtitle": "35 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_35.png",
+    "urlRadio":"https://webradio.tda.dz/Boumerdes_64K.mp3"
+  },
+  { 
+    "id": "36",
+    "title": "إذاعة الطارف",
+    "subtitle": "36 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_36.png",
+    "urlRadio":"https://webradio.tda.dz/Taref_64K.mp3"
+  },
+  { 
+    "id": "37",
+    "title": "إذاعة تندوف",
+    "subtitle": "37 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_37.png",
+    "urlRadio":"https://webradio.tda.dz/Tindouf_64K.mp3"
+  },
+  { 
+    "id": "38",
+    "title": "إذاعة تسمسيلت",
+    "subtitle": "38 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_38.png",
+    "urlRadio":"https://webradio.tda.dz/Tissemssilt_64K.mp3"
+  },
+  { 
+    "id": "39",
+    "title": "إذاعة الوادي",
+    "subtitle": "39 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_39.png",
+    "urlRadio":"https://webradio.tda.dz/ouedSouf_64K.mp3"
+  },
+  { 
+    "id": "40",
+    "title": "إذاعة خنشلة",
+    "subtitle": "40 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_40.png",
+    "urlRadio":"https://webradio.tda.dz/Khenchela_64K.mp3"
+  },
+  { 
+    "id": "41",
+    "title": "إذاعة سوق أهراس",
+    "subtitle": "41 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_41.png",
+    "urlRadio":"https://webradio.tda.dz/SoukAhras_64K.mp3"
+  },
+  { 
+    "id": "42",
+    "title": "إذاعة تيبازة",
+    "subtitle": "42 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_42.png",
+    "urlRadio":"https://webradio.tda.dz/Tipaza_64K.mp3"
+  },
+  { 
+    "id": "43",
+    "title": "إذاعة ميلة",
+    "subtitle": "43 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_43.png",
+    "urlRadio":"https://webradio.tda.dz/Mila_64K.mp3"
+  },
+  { 
+    "id": "44",
+    "title": "إذاعة عين الدفلى",
+    "subtitle": "44 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_44.png",
+    "urlRadio":"https://webradio.tda.dz/AinDefla_64K.mp3"
+  },
+  { 
+    "id": "45",
+    "title": "إذاعة النعامة",
+    "subtitle": "45 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_45.png",
+    "urlRadio":"https://webradio.tda.dz/Naama_64K.mp3"
+  },
+  { 
+    "id": "46",
+    "title": "إذاعة عين تموشنت",
+    "subtitle": "46 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_46.png",
+    "urlRadio":"https://webradio.tda.dz/AinTimouchent_64K.mp3"
+  },
+  { 
+    "id": "47",
+    "title": "إذاعة غرداية",
+    "subtitle": "47 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_47.png",
+    "urlRadio":"https://webradio.tda.dz/Ghardaia_64K.mp3"
+  },
+  { 
+    "id": "48",
+    "title": "إذاعة غليزان",
+    "subtitle": "48 _ إذاعة محلية عامة",
+    "urlImage":"assets/images/Ra_48.png",
+    "urlRadio":"https://webradio.tda.dz/Relizan_64K.mp3"
   }
-}
+]
+  
